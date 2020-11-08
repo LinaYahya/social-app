06b2667de3364bd6172d4970938997fdf405a374
Lina Ebeid <linaebe0@gmail.com>
2020-11-08 21:02:34 +0200

    query for activeAccount created

--- server/database/queries/userQueries.js
+++ server/database/queries/userQueries.js
@@ -3,3 +3,5 @@ const Users = require('../modals/Users');
 exports.checkUserByEmail = (email) => Users.find({ email });
 
 exports.createUser = (name, email, password) => Users.create({ name, email, password });
+
+exports.activeAccount = (_id) => Users.updateOne({ _id }, { active: true });
