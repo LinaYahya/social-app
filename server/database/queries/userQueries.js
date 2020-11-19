1b9d7b0040ade1cf7c5584ddbb443d3b741477f8
Lina Ebeid <linaebe0@gmail.com>
2020-11-19 21:14:46 +0200

    create friends queries

--- server/database/queries/userQueries.js
+++ server/database/queries/userQueries.js
@@ -5,3 +5,9 @@ exports.checkUserByEmail = (email) => Users.find({ email });
 exports.createUser = (name, email, password) => Users.create({ name, email, password });
 
 exports.activateAccount = (_id) => Users.updateOne({ _id }, { active: true });
+
+exports.findFriends = (_id) => Users.findOne({ _id }, 'friends');
+
+exports.addFriend = (_id, userID) => Users.updateOne({ _id }, { $push: { friends: { userID } } });
+
+exports.deleteFriend = (_id, friends) => Users.updateOne({ _id }, { friends });
