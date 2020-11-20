8018629a5c00dd8f24b12b2bba5eeab87bb8bcba
Lina Ebeid <linaebe0@gmail.com>
2020-11-20 20:57:50 +0200

    add comments

--- server/controllers/respondFriendRequest.js
+++ server/controllers/respondFriendRequest.js
@@ -6,22 +6,28 @@ module.exports = async (req, res, next) => {
   try {
     const { friendID, respond } = req.body;
     const { id } = req.user;
-
+    // validate request body
     await acceptFriendSchema.validateAsync({ friendID, respond }, { abortEarly: false });
+    // get friends list
     let { friends } = await findFriends(id);
+    // check if friend request exist
     const isFriendExist = friends.some(({ userID }) => String(userID) === friendID);
 
     if (isFriendExist) {
       if (respond === 'true') {
+        // change status to true if accept friend
         friends = friends.map((friend) => {
           if (String(friend.userID) === friendID) {
+            // eslint-disable-next-line no-param-reassign
             friend.status = 'true';
           }
           return friend;
         });
       } else {
+        // remove friend if request denied
         friends = friends.filter(({ userID }) => String(userID) !== friendID);
       }
+      // update friends list
       await updateFriends(id, friends);
       res.json({
         msg: 'update friend successfully',
