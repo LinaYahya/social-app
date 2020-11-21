a656f841b55ebfc2db8b3fe1108f962e867e0ee9
Lina Ebeid <linaebe0@gmail.com>
2020-11-21 18:29:48 +0200

    create getFriends controller

--- server/controllers/friends/getFriends.js
+++ server/controllers/friends/getFriends.js
@@ -1,7 +1,37 @@
+const Boom = require('@hapi/boom');
+const { findFriends, getUser } = require('../../database/queries/userQueries');
+
+const getFriendsData = async (arr, state) => {
+  const friendsList = arr.filter(({ status: friendStatus }) => friendStatus === state);
+  const list = await Promise.all(friendsList.map(async ({ userID }) => {
+    const user = await getUser(userID);
+    return user;
+  }));
+  return list;
+};
+
 module.exports = async (req, res, next) => {
   try {
+    const { status } = req.params;
+    const { id } = req.user;
+    const { friends } = await findFriends(id);
 
+    switch (status) {
+      case 'pending': {
+        const list = await getFriendsData(friends, 'pending');
+        res.json({ list });
+        break;
+      }
+      case 'friends': {
+        const list = await getFriendsData(friends, 'true');
+        res.json({ list });
+        break;
+      }
+      default: {
+        throw Boom.badRequest('route not exist');
+      }
+    }
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
--- server/database/queries/userQueries.js
+++ server/database/queries/userQueries.js
@@ -4,6 +4,8 @@ exports.checkUserByEmail = (email) => Users.find({ email });
 
 exports.createUser = (name, email, password) => Users.create({ name, email, password });
 
+exports.getUser = (_id) => Users.findOne({ _id }, { name: 1, email: 1, avatar: 1 });
+
 exports.activateAccount = (_id) => Users.updateOne({ _id }, { active: true });
 
 exports.findFriends = (_id) => Users.findOne({ _id }, 'friends');
