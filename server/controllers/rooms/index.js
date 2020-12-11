86ca3f9b7b34f9b2a89a976d43adb44c0d2c85ad
Lina Ebeid <linaebe0@gmail.com>
2020-12-11 19:01:35 +0200

    fix getRooms controller to send name and avatar if not exist

--- server/controllers/rooms/index.js
+++ server/controllers/rooms/index.js
@@ -1,10 +1,21 @@
+/* eslint-disable no-param-reassign */
 const { getRooms, createRoom } = require('../../database/queries/roomQueries');
+const { getUser } = require('../../database/queries/userQueries');
 
 exports.getUserRooms = async (req, res, next) => {
   try {
     const { id } = req.user;
-    const rooms = await getRooms(id);
+    let rooms = await getRooms(id);
     if (rooms.length) {
+      rooms = await Promise.all(rooms.map(async (room) => {
+        if (room.users.length === 2) {
+          const friendID = room.users.find((ele) => String(ele) !== id);
+          const { name, avatar } = await getUser(friendID);
+          room.name = name;
+          room.avatar = avatar;
+        }
+        return room;
+      }));
       res.json({ rooms });
     } else {
       res.status(404).json({ rooms, msg: 'no data found' });
