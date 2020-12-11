0caae493da54b253d78e6a5bd48c235bae2c0617
Lina Ebeid <linaebe0@gmail.com>
2020-12-11 19:50:25 +0200

    add onClick handler to room list

--- client/src/components/Rooms/index.js
+++ client/src/components/Rooms/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable prefer-template */
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRooms } from '../../slices/roomsSlice';
 import './style.css';
 
-function Rooms() {
+function Rooms({ setChatID }) {
   const { rooms } = useSelector((state) => state.rooms);
   const dispatch = useDispatch();
 
@@ -14,7 +16,7 @@ function Rooms() {
   return (
     <div className="conversations">
       {rooms?.map(({ _id, name, avatar }) => (
-        <div className="room" key={_id}>
+        <div className="room" role="button" tabIndex={0} key={_id} onClick={() => setChatID(_id)}>
           <img src={'https://res.cloudinary.com/dacf3uopo/image/upload/c_scale,h_50,r_500,w_50/v1605737001/' + avatar} alt={name} />
           <span>{name}</span>
         </div>
@@ -24,7 +26,7 @@ function Rooms() {
 }
 
 Rooms.propTypes = {
-
+  setChatID: PropTypes.func.isRequired,
 };
 
 export default Rooms;
